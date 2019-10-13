fb52bdca4896cfae53addd21c3bde4786011bfe4
Aswin Bhim Nath <aswismail@gmail.com>
2019-10-13 14:16:16 +0530

    Error Fixed

--- app/src/main/java/com/project/androalert/MainActivity.java
+++ app/src/main/java/com/project/androalert/MainActivity.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.project.androalertdialog.AndroAlert;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        new AndroAlert(this).alertMessageSuccess("Haaas");
     }
 }
